b3be56d81d1e90f32e8ca134f198199a051756d5
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-04-02 15:19:32 -0500

    feat: Initial Template

---
+++ README.md
@@ -0,0 +1,23 @@
+# Watermelon Code Bot
+
+---
+
+## Setup
+
+```shell
+pipenv shell
+
+pipenv install
+
+pipenv install --dev
+```
+
+Read the .env.example and rename to .env and changes the values
+
+### Run the bot
+
+```shell
+
+pipenv run start
+
+```
---
+++ src/main.py
@@ -0,0 +1,30 @@
+from os import getenv
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# Load enviroments from .env file
+load_dotenv()
+
+# Create bot client
+prefix = getenv("BOT_PREFIX")
+description = getenv("BOT_DESCRIPTION")
+client = commands.Bot(command_prefix=prefix, description=description)
+
+
+# On bot ready
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+# Load the extensions or cogs
+cogs = ["cogs.ping"]
+for i in cogs:
+    try:
+        client.load_extension(i)
+    except Exception as err:
+        print(f"An error has occurred {err}")
+
+# Run the bot
+if __name__ == "__main__":
+    client.run(getenv("BOT_TOKEN"))
